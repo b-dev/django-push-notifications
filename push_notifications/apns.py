a5d404c6392709a0860a9f4927e020cf53fa79da
Marco Minutoli <info@marcominutoli.it>
2014-09-29 16:12:55 +0200

    test management prune devices

--- push_notifications/apns.py
+++ push_notifications/apns.py
@@ -7,17 +7,33 @@ https://developer.apple.com/library/ios/#documentation/NetworkingInternet/Concep
 import json
 import struct
 import urllib2
+import ssl
+import socket
+import time
+from contextlib import closing
 from binascii import unhexlify
 from django.conf import settings
-from . import NotificationError, PUSH_NOTIFICATIONS_SETTINGS as SETTINGS
+from . import NotificationError
+from .settings import PUSH_NOTIFICATIONS_SETTINGS as SETTINGS
 from .exceptions import NotificationPayloadSizeExceeded, InvalidPassPhrase
 
 
 class APNSError(NotificationError):
     pass
 
+
+class APNSServerError(APNSError):
+    def __init__(self, status, identifier):
+        super(APNSServerError, self).__init__(status, identifier)
+        self.status = status
+        self.identifier = identifier
+
+
 class APNSDataOverflow(APNSError):
     pass
+
+
+APNS_MAX_NOTIFICATION_SIZE = 256
 
 SETTINGS.setdefault("APNS_PORT", 2195)
 if settings.DEBUG:
@@ -25,10 +41,8 @@ if settings.DEBUG:
 else:
     SETTINGS.setdefault("APNS_HOST", "gateway.push.apple.com")
 
-APNS_MAX_NOTIFICATION_SIZE = 256
 
-
-def _apns_create_socket():
+def _apns_create_socket(address_tuple):
     """
     Establishes an encrypted SSL socket connection to the service.
     After connecting the socket can be written to or read from.
@@ -57,7 +71,7 @@ def _apns_create_socket():
     context.use_certificate(cert)
     context.use_privatekey(pkey)
     connection = OpenSSL.SSL.Connection(context, sock)
-    connection.connect((SETTINGS["APNS_HOST"], SETTINGS["APNS_PORT"]))
+    connection.connect(address_tuple)
     connection.set_connect_state()
     connection.do_handshake()
     return connection
@@ -71,7 +85,8 @@ def _apns_create_socket_old():
     sock = socket()
     certfile = SETTINGS.get("APNS_CERTIFICATE")
     if not certfile:
-        raise ImproperlyConfigured('You need to set PUSH_NOTIFICATIONS_SETTINGS["APNS_CERTIFICATE"] to send messages through APNS.')
+        raise ImproperlyConfigured(
+            'You need to set PUSH_NOTIFICATIONS_SETTINGS["APNS_CERTIFICATE"] to send messages through APNS.')
 
     try:
         f = open(certfile, "r")
@@ -85,15 +100,63 @@ def _apns_create_socket_old():
 
     return sock
 
-def _apns_pack_message(token, data):
-    format = "!cH32sH%ds" % (len(data))
-    return struct.pack(format, b"\0", 32, unhexlify(token), len(data), data)
 
-def _apns_send(token, alert, badge=0, sound="chime", content_available=False, action_loc_key=None, loc_key=None, loc_args=[], extra={}, socket=None):
+def _apns_create_socket_to_push():
+    return _apns_create_socket((SETTINGS["APNS_HOST"], SETTINGS["APNS_PORT"]))
+
+
+def _apns_create_socket_to_feedback():
+    return _apns_create_socket((SETTINGS["APNS_FEEDBACK_HOST"], SETTINGS["APNS_FEEDBACK_PORT"]))
+
+
+def _apns_pack_frame(token_hex, payload, identifier, expiration, priority):
+    token = unhexlify(token_hex)
+    # |COMMAND|FRAME-LEN|{token}|{payload}|{id:4}|{expiration:4}|{priority:1}
+    frame_len = 3 * 5 + len(token) + len(payload) + 4 + 4 + 1  # 5 items, each 3 bytes prefix, then each item length
+    frame_fmt = "!BIBH%ssBH%ssBHIBHIBHB" % (len(token), len(payload))
+    frame = struct.pack(
+        frame_fmt,
+        2, frame_len,
+        1, len(token), token,
+        2, len(payload), payload,
+        3, 4, identifier,
+        4, 4, expiration,
+        5, 1, priority)
+
+    return frame
+
+
+def _apns_check_errors(sock):
+    timeout = SETTINGS["APNS_ERROR_TIMEOUT"]
+    if timeout is None:
+        return  # assume everything went fine!
+    saved_timeout = sock.gettimeout()
+    try:
+        sock.settimeout(timeout)
+        data = sock.recv(6)
+        if data:
+            command, status, identifier = struct.unpack("!BBI", data)
+            # apple protocol says command is always 8. See http://goo.gl/ENUjXg
+            assert command == 8, "Command must be 8!"
+            if status != 0:
+                raise APNSServerError(status, identifier)
+    except socket.timeout:  # py3
+        pass
+    except ssl.SSLError as e:  # py2
+        if "timed out" not in e.message:
+            raise
+    finally:
+        sock.settimeout(saved_timeout)
+
+
+def _apns_send(token, alert, badge=None, sound=None, category=None, content_available=False,
+               action_loc_key=None, loc_key=None, loc_args=[], extra={}, identifier=0,
+               expiration=None, priority=10, socket=None):
     data = {}
+    aps_data = {}
 
     if action_loc_key or loc_key or loc_args:
-        alert = {"body": alert}
+        alert = {"body": alert} if alert else {}
         if action_loc_key:
             alert["action-loc-key"] = action_loc_key
         if loc_key:
@@ -101,53 +164,119 @@ def _apns_send(token, alert, badge=0, sound="chime", content_available=False, ac
         if loc_args:
             alert["loc-args"] = loc_args
 
-    data["alert"] = alert
+    if alert is not None:
+        aps_data["alert"] = alert
 
-    if badge:
-        data["badge"] = badge
+    if badge is not None:
+        aps_data["badge"] = badge
 
-    if sound:
-        data["sound"] = sound
+    if sound is not None:
+        aps_data["sound"] = sound
+
+    if category is not None:
+        aps_data["category"] = category
 
     if content_available:
-        data["content-available"] = 1
+        aps_data["content-available"] = 1
 
+    data["aps"] = aps_data
     data.update(extra)
 
     # convert to json, avoiding unnecessary whitespace with separators
-    data = json.dumps({"aps": data}, separators=(",", ":"))
+    json_data = json.dumps(data, separators=(",", ":")).encode("utf-8")
 
-    if len(data) > APNS_MAX_NOTIFICATION_SIZE:
+    if len(json_data) > APNS_MAX_NOTIFICATION_SIZE:
         raise APNSDataOverflow("Notification body cannot exceed %i bytes" % (APNS_MAX_NOTIFICATION_SIZE))
 
-    data = _apns_pack_message(token, data)
+    # if expiration isn't specified use 1 month from now
+    expiration_time = expiration if expiration is not None else int(time.time()) + 2592000
+
+    frame = _apns_pack_frame(token, json_data, identifier, expiration_time, priority)
 
     if socket:
-        socket.write(data)
+        socket.write(frame)
     else:
-        socket = _apns_create_socket()
-        socket.write(data)
-        socket.close()
+        with closing(_apns_create_socket_to_push()) as socket:
+            socket.write(frame)
+            _apns_check_errors(socket)
 
 
-def apns_send_message(registration_id, data, **kwargs):
+def _apns_read_and_unpack(socket, data_format):
+    length = struct.calcsize(data_format)
+    data = socket.recv(length)
+    if data:
+        return struct.unpack_from(data_format, data, 0)
+    else:
+        return None
+
+
+def _apns_receive_feedback(socket):
+    expired_token_list = []
+
+    # read a timestamp (4 bytes) and device token length (2 bytes)
+    header_format = '!LH'
+    has_data = True
+    while has_data:
+        try:
+            # read the header tuple
+            header_data = _apns_read_and_unpack(socket, header_format)
+            if header_data is not None:
+                timestamp, token_length = header_data
+                # Unpack format for a single value of length bytes
+                token_format = '%ss' % token_length
+                device_token = _apns_read_and_unpack(socket, token_format)
+                if device_token is not None:
+                    # _apns_read_and_unpack returns a tuple, but
+                    # it's just one item, so get the first.
+                    expired_token_list.append((timestamp, device_token[0]))
+            else:
+                has_data = False
+        except ssl.SSLError as e:  # py2
+            if "timed out" not in e.message:
+                raise
+
+    return expired_token_list
+
+
+def apns_send_message(registration_id, alert, **kwargs):
     """
     Sends an APNS notification to a single registration_id.
     This will send the notification as form data.
     If sending multiple notifications, it is more efficient to use
     apns_send_bulk_message()
-    Note that \a data should always be a string.
+
+    Note that if set alert should always be a string. If it is not set,
+    it won't be included in the notification. You will need to pass None
+    to this for silent notifications.
     """
 
-    return _apns_send(registration_id, data, **kwargs)
+    return _apns_send(registration_id, alert, **kwargs)
 
-def apns_send_bulk_message(registration_ids, data, **kwargs):
+
+def apns_send_bulk_message(registration_ids, alert, **kwargs):
     """
     Sends an APNS notification to one or more registration_ids.
     The registration_ids argument needs to be a list.
-    """
-    socket = _apns_create_socket()
-    for registration_id in registration_ids:
-        _apns_send(registration_id, data, socket=socket, **kwargs)
 
-    socket.close()
+    Note that if set alert should always be a string. If it is not set,
+    it won't be included in the notification. You will need to pass None
+    to this for silent notifications.
+    """
+    with closing(_apns_create_socket_to_push()) as socket:
+        for identifier, registration_id in enumerate(registration_ids):
+            _apns_send(registration_id, alert, identifier=identifier, socket=socket, **kwargs)
+        _apns_check_errors(socket)
+
+
+def apns_fetch_inactive_ids():
+    """
+    Queries the APNS server for id's that are no longer active since
+    the last fetch
+    """
+    with closing(_apns_create_socket_to_feedback()) as socket:
+        inactive_ids = []
+        # Maybe we should have a flag to return the timestamp?
+        # It doesn't seem that useful right now, though.
+        for tStamp, registration_id in _apns_receive_feedback(socket):
+            inactive_ids.append(registration_id.encode('hex'))
+        return inactive_ids
--- push_notifications/models.py
+++ push_notifications/models.py
@@ -68,3 +68,9 @@ class APNSDevice(Device):
     def send_message(self, message, **kwargs):
         from .apns import apns_send_message
         return apns_send_message(registration_id=self.registration_id, data=message, **kwargs)
+
+# This is an APNS-only function right now, but maybe GCM will implement it
+# in the future.  But the definition of 'expired' may not be the same. Whatevs
+def get_expired_tokens():
+	from .apns import apns_fetch_inactive_ids
+	return apns_fetch_inactive_ids()
